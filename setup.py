8727e87d49bf7bea25c19935d219a90bc261b836
decoding-life <akhil.edu1@gmail.com>
2015-12-02 22:13:14 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+
+
+from distutils.core import setup 
+
+
+setup( name="test",
+	version = "02",
+	description = "tada",
+	author = "Akhil",
+	packages = ['package'], 
+	url = "https://github.com/decoding-life/distpypi",
+	keywords = ['test','testpypi']
+)
